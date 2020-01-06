572ca1408dfdfdcde1470ee367b44e7db921022c
Nick Gadomskiy <ngadomskiy@gmail.com>
2020-01-06 14:52:02 -0500

    created new functions file and added the require file for index

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+require('functions.php');
+
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,5 +26,9 @@ error_reporting(E_ALL);
 </head>
 <body>
     <h1>Pair Program 1</h1>
+    <?php
+        printArr($numbers);
+    ?>
+
 </body>
 </html>
