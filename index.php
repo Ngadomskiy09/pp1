4c8df414a81a1930f6d780aaceb18e223de23678
Nick Gadomskiy <ngadomskiy@gmail.com>
2020-01-06 14:39:01 -0500

    created index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Nick Gadomskiy
+ * @date 1-6-20
+ * @url http://ngadomskiy.greenriverdev.com/IT328/pp1/
+ * Pair Program 1
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PP1</title>
+</head>
+<body>
+    <h1>Pair Program 1</h1>
+</body>
+</html>
